3b82b22dab0be50278231954a7347dce92092074
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-09-06 19:20:24 +0200

    Test runtime::kill && close methods

--- tests/RuntimeTest.php
+++ tests/RuntimeTest.php
@@ -7,7 +7,9 @@ use React\Promise\ExtendedPromiseInterface;
 use WyriHaximus\AsyncTestUtilities\AsyncTestCase;
 use WyriHaximus\React\Parallel\FutureToPromiseConverter;
 use WyriHaximus\React\Parallel\Runtime;
+use parallel\Runtime\Error\Closed;
 use function Safe\sleep;
+use function WyriHaximus\React\timedPromise;
 
 /**
  * @internal
@@ -59,5 +61,57 @@ final class RuntimeTest extends AsyncTestCase
         $three = $this->await($promise, $loop, 3.3);
 
         self::assertSame(3, $three);
+    }
+
+    public function testWeClosedTheThread(): void
+    {
+        self::expectException(Closed::class);
+        self::expectExceptionMessage('Runtime closed');
+
+        $loop = Factory::create();
+        $runtime = new Runtime(new FutureToPromiseConverter($loop), \dirname(__DIR__) . '/vendor/autoload.php');
+
+        /** @var ExtendedPromiseInterface $promise */
+        $promise = timedPromise($loop, 1, $runtime)->then(function (Runtime $runtime) {
+            return $runtime->run(function (): int {
+                return 3;
+            });
+        });
+
+        $loop->futureTick(function () use ($runtime): void {
+            $runtime->close();
+        });
+
+        try {
+            $this->await($promise, $loop, 3.3);
+        } catch (\Exception $exception) {
+            throw $exception->getPrevious();
+        }
+    }
+
+    public function testWeKilledTheThread(): void
+    {
+        self::expectException(Closed::class);
+        self::expectExceptionMessage('Runtime closed');
+
+        $loop = Factory::create();
+        $runtime = new Runtime(new FutureToPromiseConverter($loop), \dirname(__DIR__) . '/vendor/autoload.php');
+
+        /** @var ExtendedPromiseInterface $promise */
+        $promise = timedPromise($loop, 1, $runtime)->then(function (Runtime $runtime) {
+            return $runtime->run(function (): int {
+                return 3;
+            });
+        });
+
+        $loop->futureTick(function () use ($runtime): void {
+            $runtime->kill();
+        });
+
+        try {
+            $this->await($promise, $loop, 3.3);
+        } catch (\Exception $exception) {
+            throw $exception->getPrevious();
+        }
     }
 }
